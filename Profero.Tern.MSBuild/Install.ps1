57eb08db05f11715b67362b38a34fb65cfaf6ae8
Richard Szalay <richard.szalay@profero.com>
2013-09-30 08:23:29 +1000

    Removed NuGet Install.ps1 as it wasn't being used

--- Profero.Tern.MSBuild/Install.ps1
+++
@@ -1,6 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$vsSolution = Get-Interface $dte.Solution ([EnvDTE80.Solution2])
-$vsProject = $vsSolution.AddSolutionFolder("Database")
-$projectItems = Get-Interface $vsProject.ProjectItems ([EnvDTE.ProjectItems])
-$projectItems.AddFromFile("pathToFileToAdd.txt")
