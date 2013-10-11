490768c989f78d1aab57a76e78e549d46aab46e0
Richard Szalay <richard.szalay@profero.com>
2013-10-11 16:30:46 +1100

    Added assembly/package versioning into the build script

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,11 +1,9 @@
 using System.Reflection;
-
 [assembly: AssemblyCompany("Profero")]
 [assembly: AssemblyProduct("Profero.Tern")]
-[assembly: AssemblyCopyright("Copyright © Profero 2013")]
+[assembly: AssemblyCopyright("Copyright ï¿½ Profero 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha1")]
+//[assembly: AssemblyInformationalVersion("1.0.0-alpha1")]
